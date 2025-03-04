9c6c8138feb63df8251dbef515af0e39a42b2f01
Alexander Evstratov <alexdrugev@me.com>
2025-03-04 10:15:34 +0300

    Remaking donor.

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Block{
+    public float xValue;
+    public float yValue;
+    int number;
+    public GameObject spawnedBlock;
+    TextMeshProUGUI textNumber;
+    Sprite _sprite;
+
+    public Block(float newxValue,float newyValue,GameObject newspawnedBlock,TextMeshProUGUI newtextNumber, Sprite spritePart) {
+        xValue = newxValue;
+        yValue = newyValue;
+        spawnedBlock = newspawnedBlock;
+        textNumber = newtextNumber;
+        _sprite = spritePart;
+    }
+
+    public Block BlockStartValue(float newxValue, float newyValue, GameObject newspawnedBlock, TextMeshProUGUI newtextNumber, Sprite spritePart) {
+        xValue = newxValue;
+        yValue = newyValue;
+        spawnedBlock = newspawnedBlock;
+        textNumber = newtextNumber;
+        _sprite = spritePart;
+        return this;
+    }
+
+    public void SetTextNumber(int newNumber) {
+        textNumber.text = newNumber.ToString();
+    }
+
+    public void SetNumber(int newNumber) {
+        number = newNumber;
+    }
+
+    public void SetSpawnedBlock(GameObject newSpawnedBlock) {
+        spawnedBlock = newSpawnedBlock;
+    }
+
+    public Sprite GetSprite()
+    {
+        return spawnedBlock.GetComponent<Image>().sprite;
+    }
+
+    public void SetSprite(Sprite sprite)
+    {
+        spawnedBlock.GetComponent<Image>().sprite = sprite;
+    }
+
+
+    public int GetNumber() { return number; }
+    public TextMeshProUGUI GetTextNumber() { return textNumber; }
+    public GameObject GetSpawnedBlock() { return spawnedBlock; }
+}
